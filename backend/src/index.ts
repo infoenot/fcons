b2ba2ba2d4ea48f97719571e1c44953408b15e9a
infoenot <infoenot@gmail.com>
2026-02-25 04:56:42 +0000

    fix: port type number for TypeScript

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
 app.use(cors());
 app.use(express.json());
